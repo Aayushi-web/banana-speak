e75dee70e801341202ea8376bf17baaa0d9a7abc
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-13 01:20:15 +0530

    add event listener

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("heyyyy")
+// let btnTranslate= document.querySelector("#btn-translate")
+// btnTranslate.addEventListener("click",function clickEventHanler(){
+//     console.log("clicked")
+// })
+let btn= document.querySelector("#btn-translate")
+btn.addEventListener("keyboard",function clickevent(){
+document.getElementById("btn-translate").style.color="yellow"
+})
