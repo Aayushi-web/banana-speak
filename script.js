d38bfe1c78adef4782357a53164d37c521598d1d
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-13 15:13:39 +0530

    input output processes done

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 // btnTranslate.addEventListener("click",function clickEventHanler(){
 //     console.log("clicked")
 // })
-let btn= document.querySelector("#btn-translate")
-btn.addEventListener("click",
-function clickevent(){
-document.getElementById("btn-translate").style.background="yellow"
-console.log("input",text.value)
-})
-let text= document.querySelector("#txt-input");
+// let btn= document.querySelector("#btn-translate")
+// btn.addEventListener("click",
+// function clickevent(){
+// document.getElementById("btn-translate").style.background="yellow"
+// console.log("input",text.value)
+// })
+// let text= document.querySelector("#txt-input");
 
+
+let text= document.querySelector("textarea");
+let btn=document.querySelector("#btn-translate");
+let outputDiv= document.querySelector("#output")
+// outputDiv.innerText="Aayushi"
+// text.addEventListener("input",function txt(){
+//     console.log(text.value)
+// })
+btn.addEventListener("click",function cli(){
+    document.getElementById("btn-translate").style.background="green"
+   outputDiv.innerText="vm,m,.cbm."+text.value
+})
